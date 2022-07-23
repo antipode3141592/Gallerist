0abf239724d7bfd2c88b86d62ee3e9a81744cf0d
Sean <seanvokirkpatrick@gmail.com>
2022-07-23 11:07:05 -0400

    state machine and transition assembly added

---
+++ Assets/Scripts/StateMachines/Transition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FiniteStateMachine
+{
+    public class Transition
+    {
+        public Func<bool> Condition { get; }
+        public IState To { get; }
+
+        public Transition(IState to, Func<bool> condition)
+        {
+            To = to;
+            Condition = condition;
+        }
+    }
+
+
+}
