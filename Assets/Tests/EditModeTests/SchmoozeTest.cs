d6ca7318a332b3b6d999e56f6c99c46db8b4271e
Sean <seanvokirkpatrick@gmail.com>
2022-08-04 00:49:10 -0400

    added rules/directions to Preparation Display.  Updated look of ListItem for preparation list items.  added a couple edit mode tests for the schmooze actions

---
+++ Assets/Tests/EditModeTests/SchmoozeTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gallerist;
+using NUnit.Framework;
+
+namespace Testing.EditMode
+{
+    public class SchmoozeTest
+    {
+        Patron patron;
+        Artist artist;
+
+        [SetUp]
+        public void Setup()
+        {
+            patron = TestFactories.GetPatron();
+            artist = TestFactories.GetArtist();
+        }
+
+        [Test]
+        public void ChatReturnsRevealedList()
+        {
+            List<ITrait> revealedTraits = Schmooze.Chat(patron: patron);
+            if (revealedTraits is null)
+                Assert.Fail();
+            Assert.IsTrue(revealedTraits.Count > 0);
+        }
+
+        [Test]
+        public void ChatFailsWhenAllTraitsRevealed()
+        {
+            foreach (var trait in patron.AestheticTraits)
+                trait.IsKnown = true;
+            foreach (var trait in patron.EmotiveTraits)
+                trait.IsKnown = true;
+
+            List<ITrait> revealedTraits = Schmooze.Chat(patron: patron);
+            Assert.IsTrue(revealedTraits is null);
+            Assert.IsTrue(patron.AllTraitsKnown);
+        }
+
+        [Test]
+        public void NudgeReducesThresholdByOne()
+        {
+            Tuple<int, int> prevalue = new(patron.EmotiveThreshold, patron.AestheticThreshold);
+            var result = Schmooze.Nudge(patron);
+            Assert.IsTrue(prevalue.Item1 - patron.EmotiveThreshold == 1 || prevalue.Item2 - patron.AestheticThreshold == 1);
+        }
+
+        [Test]
+        public void NudgeReducesThresholdByTwoWhenArtBuyer()
+        {
+            //bool didBuyArt = patron.BuyArt(TestFactories.GetArt());
+            //Assert.IsTrue(patron.Acquisitions.Count == 1);
+
+        }
+    }
+}
---
+++ Assets/Tests/EditModeTests/TestFactories.cs
@@ -0,0 +1,151 @@
+﻿using Gallerist;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing.EditMode
+{
+    public class TestFactories
+    {
+        public static string defaultArtistName = "Arty McArtist";
+
+        public static Patron GetPatron()
+        {
+            return new Patron(firstName: "Polly",
+                lastInitial: "P.",
+                portrait: null,
+                isSubscriber: false,
+                aestheticTraits: new()
+                {
+                    new PatronTrait(
+                        name: "ATrait1",
+                        value: 5,
+                        isKnown: false,
+                        traitType: TraitType.Aesthetic)
+                    ,
+                    new PatronTrait(
+                        name: "ATrait2",
+                        value: 5,
+                        isKnown: false,
+                        traitType: TraitType.Aesthetic),
+                    new PatronTrait(
+                        name: "ATrait3",
+                        value: 5,
+                        isKnown: false,
+                        traitType: TraitType.Aesthetic)
+                },
+                emotiveTraits: new()
+                {
+                    new PatronTrait(
+                        name: "ETrait1",
+                        value: 5,
+                        isKnown: false,
+                        traitType: TraitType.Emotive)
+                    ,
+                    new PatronTrait(
+                        name: "ETrait2",
+                        value: 5,
+                        isKnown: false,
+                        traitType: TraitType.Emotive),
+                    new PatronTrait(
+                        name: "ETrait3",
+                        value: 5,
+                        isKnown: false,
+                        traitType: TraitType.Emotive)
+                },
+                acquisitions: new()
+                {
+
+                },
+                aestheticThreshold: 8,
+                emotiveThreshold: 8,
+                boredomThreshold: 2);
+        }
+
+        public static Artist GetArtist()
+        {
+            return new Artist(name: defaultArtistName,
+                favoredAestheticTraits: new()
+                {
+                    new ArtistTrait(
+                    name: "ATrait1",
+                    value: 5,
+                    isKnown: false,
+                    traitType: TraitType.Aesthetic),
+                    new ArtistTrait(
+                    name: "ATrait2",
+                    value: 5,
+                    isKnown: false,
+                    traitType: TraitType.Aesthetic),
+                    new ArtistTrait(name: "ATrait3",
+                    value: 5,
+                    isKnown: false,
+                    traitType: TraitType.Aesthetic)
+                },
+                favoredEmotiveTraits: new()
+                {
+                    new ArtistTrait(
+                    name: "ETrait1",
+                    value: 5,
+                    isKnown: false,
+                    traitType: TraitType.Emotive),
+                    new ArtistTrait(
+                    name: "ETrait2",
+                    value: 5,
+                    isKnown: false,
+                    traitType: TraitType.Emotive),
+                    new ArtistTrait(name: "ETrait3",
+                    value: 5,
+                    isKnown: false,
+                    traitType: TraitType.Emotive)
+                },
+                portrait: null);
+        }
+
+        public static Art GetArt()
+        {
+            return new Art(
+                name: "Example Art #1",
+                description: "An incredible, innovative feat of daring!",
+                artistName: defaultArtistName,
+                aestheticQualities: new()
+                {
+                    new ArtTrait(
+                    name: "ATrait1",
+                    value: 5,
+                    isKnown: false,
+                    traitType: TraitType.Aesthetic),
+                    new ArtTrait(
+                    name: "ATrait2",
+                    value: 5,
+                    isKnown: false,
+                    traitType: TraitType.Aesthetic),
+                    new ArtTrait(name: "ATrait3",
+                    value: 5,
+                    isKnown: false,
+                    traitType: TraitType.Aesthetic)
+                },
+                emotiveQualities: new()
+                {
+                    new ArtTrait(
+                    name: "ETrait1",
+                    value: 5,
+                    isKnown: false,
+                    traitType: TraitType.Emotive),
+                    new ArtTrait(
+                    name: "ETrait2",
+                    value: 5,
+                    isKnown: false,
+                    traitType: TraitType.Emotive),
+                    new ArtTrait(name: "ETrait3",
+                    value: 5,
+                    isKnown: false,
+                    traitType: TraitType.Emotive)
+                },
+                image: null);
+        }
+
+    }
+}
